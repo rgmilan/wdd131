d4bcbe76b74cf8d4293cea4a35dea2cd8c661681
rgmilan <dj.rg.milan@gmail.com>
2026-05-07 22:17:59 +0800

    Week 1 - Initial Commit

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").innerHTML =
+    `Last Modification: ${document.lastModified}`;
