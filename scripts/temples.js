afa25326d605e9087a594a8ae3c1f89befa66025
rgmilan <dj.rg.milan@gmail.com>
2026-05-16 14:00:55 +0800

    Temples page - html, css, js, images

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const currentYear = document.querySelector('#currentyear');
+const lastModified = document.querySelector('#lastModified');
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    menuButton.classList.toggle('open');
+});
