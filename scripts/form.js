ed45d881dad07ff063b6b8afef90b0ff14483bc4
rgmilan <dj.rg.milan@gmail.com>
2026-05-30 16:31:02 +0800

    Updated placeholder text

--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,27 @@
 const products = [
     {
         id: "gc-1001",
-        name: "general construction",
+        name: "General Construction",
         averagerating: 4.8
     },
     {
         id: "aw-1002",
-        name: "architectural works",
+        name: "Architectural Works",
         averagerating: 4.6
     },
     {
         id: "cw-1003",
-        name: "civil works",
+        name: "Civil Works",
         averagerating: 4.5
     },
     {
         id: "ew-1004",
-        name: "electrical works",
+        name: "Electrical Works",
         averagerating: 4.7
     },
     {
         id: "mp-1005",
-        name: "mechanical and plumbing works",
+        name: "Mechanical and Plumbing Works",
         averagerating: 4.4
     }
 ];
@@ -30,10 +30,8 @@ const productSelect = document.querySelector("#productName");
 
 products.forEach(product => {
     const option = document.createElement("option");
-
     option.value = product.id;
     option.textContent = product.name;
-
     productSelect.appendChild(option);
 });
 
